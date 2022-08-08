4bfeb9a2b0680b96801f417fa166596717e49930
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-08-08 01:01:19 +0100

    updated metro config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = {
+module.exports = getDefaultConfig(__dirname, {
   transformer: {
     assetPlugins: ["expo-asset/tools/hashAssetFiles"],
     getTransformOptions: async () => ({
@@ -11,4 +11,4 @@ module.exports = {
       },
     }),
   },
-}
+})
