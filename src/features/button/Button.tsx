22fb0395737377b43abab6e36d362f371b2d963f
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-08-08 14:06:37 +0100

    created button component

---
+++ src/features/button/Button.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import { Pressable, StyleSheet, Text } from "react-native"
+
+type Props = {
+  onPress: () => void
+  title: string
+  textColor: string
+  bgColor: string
+}
+
+const Button = (props: Props) => {
+  return (
+    <Pressable
+      style={[
+        styles.button,
+        {
+          backgroundColor: props.bgColor,
+        },
+      ]}
+      onPress={props.onPress}
+    >
+      <Text
+        style={[
+          styles.text,
+          {
+            color: props.textColor,
+          },
+        ]}
+      >
+        {props.title}
+      </Text>
+    </Pressable>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+  },
+})
